2f2d3459acce7d3b6b7e79ff255ff11ab179197d
Mfly87 <mped@specialisterne.com>
2024-03-11 14:02:52 +0100

    Faker - added email

--- FakerData/fakerItemCreator.py
+++ FakerData/fakerItemCreator.py
@@ -27,3 +27,51 @@ class FakerItemCreator():
         _date_time = self._faker.date_between()
         _date_string = _date_time.strftime("%Y/%m/%d")
         return _date_string
+    
+    def get_fake_email(self, fullName : str, birthyear : int):
+        _domain = self._faker.free_email_domain()
+        _id = self._get_fake_email_id(fullName, birthyear)
+        return _id + "@" + _domain
+
+
+    def _nameStyle_a(self, fullName : str, filler : str):
+        _splitName = fullName.split(" ")
+        return filler.join(_splitName)
+        
+    def _nameStyle_b(self, fullName : str, filler : str):
+        _splitName = fullName.split(" ")            
+        return filler.join([_splitName[0][0],_splitName[1]])
+    
+    def _nameStyle_c(self, fullName : str, filler : str):
+        _splitName = fullName.split(" ")
+
+        _first_name = _splitName[0]
+        del _splitName[0]
+        _rest_name = "".join(_splitName)
+
+        return filler.join([_first_name[0],_rest_name[0:3]])
+    
+    def _get_fake_email_id(self, fullName : str, birthyear : int) -> str:
+        _fillers = [".","-","_",""]
+        _fillerIndex = self._faker.pyint(min_value = 0, max_value = len(_fillers)-1)
+        _filler = _fillers[_fillerIndex]
+         
+        _styleFuncs = [
+             self._nameStyle_a, 
+             self._nameStyle_b,
+             self._nameStyle_c, 
+             ]
+        _styleFuncIndex = self._faker.pyint(min_value = 0, max_value = len(_styleFuncs)-1)
+        _styleFunc = _styleFuncs[_styleFuncIndex]
+
+        _id_name = _styleFunc(fullName, _filler)
+
+        _yearStyles = [
+            "",
+            str(birthyear),
+            str(birthyear%100).zfill(2)
+        ]
+        _yearStyleIndex = self._faker.pyint(min_value = 0, max_value = len(_yearStyles)-1)
+        
+        _id_year = _yearStyles[_yearStyleIndex]
+        return _id_name + _id_year
--- FakerData/fakerUserGenerator.py
+++ FakerData/fakerUserGenerator.py
@@ -7,11 +7,17 @@ class FakerUserGenerator(FakerAbsGenerator):
             "Name",
             "Birthday",
             "Addresse",
+            "E-mail",
         ]
 
     def generate_item_data(self):
+        _fake_name = self._item_creator.get_fake_name()
+        _fake_birthday = self._item_creator.get_fake_birthday()
+        _birthyear = int(_fake_birthday[0:4])
+
         return [
-            self._item_creator.get_fake_name(),
-            self._item_creator.get_fake_birthday(),
-            self._item_creator.get_fake_address()
+            _fake_name,
+            _fake_birthday,
+            self._item_creator.get_fake_address(),
+            self._item_creator.get_fake_email(_fake_name, _birthyear)
         ]
