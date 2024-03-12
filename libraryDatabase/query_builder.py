0f92ac9a988ce8398066b9b23dfd45dfec7f7148
Niels87 <niels.slein@gmail.com>
2024-03-12 19:52:58 +0100

    minor fix

--- libraryDatabase/query_builder.py
+++ libraryDatabase/query_builder.py
@@ -1,21 +1,24 @@
 
 class QueryBuilder(object):
     
-    # BOOKCOPIES: table of individual books with status
-    # BOOKTITLES: table of titles with author, year etc. info
+
     
     
     # Get all books that contains title
     @staticmethod
     def query_title(title: str) -> str:
-        base = "SELECT {} FROM {} {}"
-        columns = "ID, TITLE, AUTHOR, PUBLISHINGYEAR, ISBN, AVAILABILITY"
-        table1 = "BOOKS"
-        on = "WHERE BOOKS.TITLE LIKE %" + title + "%"
-        #query = base.format(columns, table1, on)
         query = "SELECT ID, TITLE, AUTHOR, PUBLISHINGYEAR, ISBN, AVAILABILITY FROM BOOKS WHERE BOOKS.TITLE LIKE " + f"\"%{title}%\""
-        print("\n" + query + "\n")
         return query
+    
+    @staticmethod
+    def query_author(author: str) -> str:
+        query = "SELECT ID, TITLE, AUTHOR, PUBLISHINGYEAR, ISBN, AVAILABILITY FROM BOOKS WHERE BOOKS.AUTHOR LIKE " + f"\"%{author}%\""
+        return query
+    
+    
+    
+    # BOOKCOPIES: table of individual books with status
+    # BOOKTITLES: table of titles with author, year etc. info
     
     # @staticmethod
     # def query_title(title: str) -> str:
@@ -27,36 +30,36 @@ class QueryBuilder(object):
     #     query = base.format(columns, table1, table2, on)
     #     return query
     
-    # Get all books by an author
-    @staticmethod
-    def query_author(author: str) -> str:
-        base = "SELECT {} FROM {} JOIN {} {}"
-        columns = "TITLE, AUTHOR, YEAR, STATUS"
-        table1 = "BOOKCOPIES"
-        table2 = "BOOKTITLES"
-        on = "ON BOOKTITLES.AUTHOR=" + author
-        query = base.format(columns, table1, table2, on)
-        return query
+    # # Get all books by an author
+    # @staticmethod
+    # def query_author(author: str) -> str:
+    #     base = "SELECT {} FROM {} JOIN {} {}"
+    #     columns = "TITLE, AUTHOR, YEAR, STATUS"
+    #     table1 = "BOOKCOPIES"
+    #     table2 = "BOOKTITLES"
+    #     on = "ON BOOKTITLES.AUTHOR=" + author
+    #     query = base.format(columns, table1, table2, on)
+    #     return query
     
-    # Get info related to a user
-    @staticmethod
-    def query_user(user_id: int) -> str:
-        base = "SELECT {} FROM {} WHERE ID={}"
-        columns = "NAME, ADDRESS"
-        table1 = "USERS"
-        query = base.format(columns, table1, user_id)
-        return query
+    # # Get info related to a user
+    # @staticmethod
+    # def query_user(user_id: int) -> str:
+    #     base = "SELECT {} FROM {} WHERE ID={}"
+    #     columns = "NAME, ADDRESS"
+    #     table1 = "USERS"
+    #     query = base.format(columns, table1, user_id)
+    #     return query
     
     
-    # Update status of an individual copy of a book
-    @staticmethod
-    def update_status(book_id: int, new_status: str):
-        # table of individual books: BOOKCOPIES
-        # field of book status: STATUS
-        # ID of book = ID
-        base = "UPDATE BOOKCOPIES SET STATUS={} WHERE ID={}"
-        query = base.format(new_status, book_id)
-        return query
+    # # Update status of an individual copy of a book
+    # @staticmethod
+    # def update_status(book_id: int, new_status: str):
+    #     # table of individual books: BOOKCOPIES
+    #     # field of book status: STATUS
+    #     # ID of book = ID
+    #     base = "UPDATE BOOKCOPIES SET STATUS={} WHERE ID={}"
+    #     query = base.format(new_status, book_id)
+    #     return query
         
 
     
