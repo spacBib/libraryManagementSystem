bb72d71d5c86adf114102873d09780589564a2de
Mfly87 <mped@specialisterne.com>
2024-03-12 14:52:44 +0100

    commiting due to interruption

---
+++ userInterface/userInteraction.py
@@ -0,0 +1,11 @@
+from libraryUsers.library_user import LibraryUser
+
+
+class UserInteraction():
+    def __init__(self, user : LibraryUser) -> None:
+        self._user = user
+
+    def get_search_strategies(self):
+        pass
+
+    
