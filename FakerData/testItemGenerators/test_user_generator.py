43610f49ed6483b2e836229e5118708076881f9e
Mfly87 <mped@specialisterne.com>
2024-03-12 16:04:42 +0100

    added test user creation

--- FakerData/testItemGenerators/test_user_generator.py
+++ FakerData/testItemGenerators/test_user_generator.py
@@ -1,11 +1,19 @@
 from .abs_test_item_generator import absTestItemGenerator
 
+from libraryUsers.library_user import LibraryUser
 from libraryUsers.library_user_factory import LibraryUserFactory
 from ..faker_user_generator import FakerUserGenerator
 
 class absTestItemGenerator(absTestItemGenerator):
 
-    def create():
-        factory = FakerUserGenerator()
-        userData = factory.generate_item_data()
+    def create() -> LibraryUser:
+        faker = FakerUserGenerator()
+        userData = faker.generate_item_data()
+
+        cleanedData = []
+        for data in userData:
+            cleanedData.append(data.replace("\"",""))
+
+        user = LibraryUserFactory.create(1, cleanedData[0], cleanedData[1], cleanedData[2])
+        return user
         
