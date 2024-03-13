af01ec62db92eea7547c0e378ff2db4668e97f56
Niels87 <niels.slein@gmail.com>
2024-03-13 14:51:02 +0100

    search request handling works

--- requestAndResult/request_abc.py
+++ requestAndResult/request_abc.py
@@ -3,11 +3,7 @@ from abc import ABC, abstractmethod
 class RequestABC(ABC):
     
     @abstractmethod
-    def __init__(self, request_type: str):
-        self._request_type = request_type
-    
-    @property
-    def request_type(self):
-        return self._request_type
+    def __init__(self):
+        pass
     
     
--- requestAndResult/request_handler.py
+++ requestAndResult/request_handler.py
@@ -1,17 +1,24 @@
-from request_abc import RequestABC
+from requestAndResult.request_abc import RequestABC
 from libraryDatabase.info_getter import InfoGetter
 from libraryDatabase.database_connector import ConnectToDatabase
-
+from requestAndResult.search.search_request import SearchRequest
+from requestAndResult.search.search_result import SearchResult
+#from search.search_factory import SearchFactory
+from requestAndResult.factory_finder import FactoryFinder
+import requestAndResult.result_abc
 class RequestHandler(object):
     
     def __init__(self) -> None:
-        self._info_getter = InfoGetter()
+        db_connecter = ConnectToDatabase()
+        sql_connection = db_connecter.connect_to_database("library_management_system")
+        self._info_getter = InfoGetter(sql_connection)
     
     
-    def handle_request(self, request: RequestABC):
-        match request._request_type:
+    def handle_request(self, request: RequestABC) -> requestAndResult.result_abc.ResultABC:
+        match request.request_type:
             case "search":
                 result = self._search_request(request)
+                return result
             case "loan":
                 result = self._loan_request(request)
             case "reservation":
@@ -19,8 +26,11 @@ class RequestHandler(object):
             case "return":
                 result = self._return_request(request)
     
-    def _search_request(self, request: RequestABC):
-        pass
+    def _search_request(self, request: SearchRequest) -> SearchResult:
+        print("\nsearch request handled")
+        result_list = self._info_getter.search_title(request.search_str)
+        factory = FactoryFinder().get_concrete_factory("search")
+        return factory.create_result(request, result_list)
     
     def _loan_request(self, request: RequestABC):
         pass
