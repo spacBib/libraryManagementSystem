4e2fd62e0a0e6e1ef710fa4df9ad20921b375481
Mfly87 <mped@specialisterne.com>
2024-03-14 08:29:53 +0100

    Added basic loan, database doesnt update...

--- userInterface/userActions/user_action_fake_loan.py
+++ userInterface/userActions/user_action_fake_loan.py
@@ -1,32 +1,24 @@
 from .abs_user_action import AbsUserAction
 
 from libraryBooks.library_book import LibraryBook
-from libraryUsers.library_user import LibraryUser
 
 from userInterface.user_choice_selector import UserChoiceSelector
 
-from requestAndResult.loan.loan_factory import LoanFactory
-from requestAndResult.loan.loan_result import LoanResult
-from requestAndResult.request_handler import RequestHandler
-
-
-class UserActionLoan(AbsUserAction):
+class UserActionFakeLoan(AbsUserAction):
     
     @property
     def name(self) -> str:
-        return "Loan"
+        return "FakeLoan"
     
     @property
     def priority(self) -> int:
-        return 0
+        return 0 + 1000
     
     def is_usable(self, userInteraction) -> bool:
         return userInteraction.previous_search_result
 
     def select_action(self, userInteraction) -> None:
         print("Which book do you want to loan?")       
-
-        _user : LibraryUser = userInteraction.user
 
         _book_list : list[LibraryBook] = userInteraction.previous_search_result
         _name_list = ["None"]
@@ -39,16 +31,7 @@ class UserActionLoan(AbsUserAction):
         if _choice_index < 0:
             return
 
-        _selected_book : LibraryBook = _book_list[_choice_index]
-
-
-
-        fact = LoanFactory()
-        req = fact.create_request(_selected_book.id, _user.id)
-        
-        req_handler = RequestHandler()
-        
-        result : LoanResult = req_handler.handle_request(req)
+        _selected_book = _book_list[_choice_index]
 
         userInteraction.user.add_book(_selected_book)
         userInteraction.set_prev_search_results([])
